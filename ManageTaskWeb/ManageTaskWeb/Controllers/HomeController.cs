9a9c17d9bd397eb4e43fcf6b110d3b7587aa6ec9
davidnguyennq <nguyennhatquan.dev@gmail.com>
2024-10-11 16:50:11 +0700

    AddDangNhap

---
+++ ManageTaskWeb/ManageTaskWeb/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManageTaskWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+
+    }
+}
