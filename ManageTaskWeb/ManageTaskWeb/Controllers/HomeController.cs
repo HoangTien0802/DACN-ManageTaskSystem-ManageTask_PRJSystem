5335d6d2cbdcb256682884f12d3fc426eb0e8d97
davidnguyennq <nguyennhatquan.dev@gmail.com>
2024-10-23 23:56:49 +0700

    Add DSProject + Fix Layout

--- ManageTaskWeb/ManageTaskWeb/Controllers/HomeController.cs
+++ ManageTaskWeb/ManageTaskWeb/Controllers/HomeController.cs
@@ -20,5 +20,9 @@ namespace ManageTaskWeb.Controllers
         {
             return View();
         }
+        public ActionResult DSProject()
+        {
+            return View();
+        }
     }
 }
